69219f6d4830b5e8a6259cd58ea31e342667e54f
Anurag-Raut <rautab0@gmail.com>
2023-09-14 18:01:40 +0530

    app-router-migration

---
+++ packages/nextjs/app/api/getAllRealEstatesWithAddress/route.ts
@@ -0,0 +1,32 @@
+import { ObjectId } from 'mongodb';
+
+import { NextResponse } from 'next/server';
+import { connectToDatabase } from '~~/servers/connect';
+
+export async function POST(req:Request) {
+    try {
+        
+        let {address}=await req.json();
+        console.log(address,'address')
+   
+        const db=await connectToDatabase('OpenEstate_properties');
+        if(!db){
+            return NextResponse.json(({ error: 'not connected to db' }),{status:400});
+        }
+        const coll=db.collection('properties');
+        const documents = await coll.find({
+            owners: {
+              $elemMatch: {
+                $eq: address
+              }
+            }
+          }).toArray();
+        console.log('documents',documents);
+      
+          return NextResponse.json({ data:documents },{status:200});
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error },{status:500});
+    }
+}

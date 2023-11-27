57ee636195c9ac775835ba28dc95ee1e7c484de0
Atharva Desai <atharvadesai.work@gmail.com>
2023-11-27 13:01:33 +0530

    worked on the filters

--- packages/nextjs/app/utils/fetchApi.js
+++ packages/nextjs/app/utils/fetchApi.js
@@ -8,11 +8,11 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
 
-  const key = process.env.NEXT_PUBLIC_RAPID_API_KEY;
+  // const key = process.env.NEXT_PUBLIC_RAPID_API_KEY;
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': key ,
+      'x-rapidapi-key': '' ,
     },
   });
     

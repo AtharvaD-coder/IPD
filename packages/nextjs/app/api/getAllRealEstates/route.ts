55941e40d21bcb3e51a689948077a48d9b5d4066
Anurag-Raut <rautab0@gmail.com>
2023-11-30 09:28:52 +0530

    fixed-chakr

--- packages/nextjs/app/api/getAllRealEstates/route.ts
+++ packages/nextjs/app/api/getAllRealEstates/route.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongodb';
 
 import { NextResponse } from 'next/server';
+import { baseUrl, fetchApi } from '~~/app/utils/fetchApi';
 import { connectToDatabase } from '~~/servers/connect';
 
 export async function GET(req:Request) {
@@ -15,6 +16,21 @@ export async function GET(req:Request) {
         const coll=db.collection('properties');
         const documents = await coll.find({}).toArray();
         console.log('documents',documents);
+        const fetchAdditionalData = async (doc:any) => {
+            const d = await fetchApi(`${baseUrl}/properties/detail?externalID=${doc.externalID}`);
+            return { ...doc, ...d };
+        };
+
+        // Use Promise.all to perform asynchronous operations concurrently
+        // const updatedDocuments = await Promise.all(documents.map(fetchAdditionalData));
+
+        // // Save the updated documents back to the database
+        // for (const updatedDoc of updatedDocuments) {
+        //     await coll.updateOne(
+        //         { _id: updatedDoc._id }, // Assuming _id is the unique identifier of each document
+        //         { $set: updatedDoc }
+        //     );
+        // }
    
       
           return NextResponse.json({ data:documents },{status:200});
